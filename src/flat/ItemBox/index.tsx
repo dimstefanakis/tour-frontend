1c0d9f4511552023ec61bc9de9293461ddcb6ea8
Dimitris <jimstef@outlook.com>
2023-01-04 18:36:30 +0200

    removed collapse arrow when not collapsed

--- src/flat/ItemBox/index.tsx
+++ src/flat/ItemBox/index.tsx
@@ -1,4 +1,11 @@
-import { Paper, Collapse, Text, Button,Container,Center } from "@mantine/core";
+import {
+  Paper,
+  Collapse,
+  Text,
+  Button,
+  Container,
+  Center,
+} from "@mantine/core";
 import { useHover } from "@mantine/hooks";
 
 function ItemBox({
@@ -35,6 +42,7 @@ function ItemBox({
       radius={hovered ? "md" : "sm"}
       p="lg"
       withBorder
+      onClick={!collapsed ? onClick : undefined}
       style={{
         minWidth: "50%",
         transition: "box-shadow 200ms ease, border-radius 200ms ease",
@@ -45,18 +53,22 @@ function ItemBox({
       {children}
       {CollapsedComponent && (
         <Collapse in={!!collapsed}>{CollapsedComponent}</Collapse>
-        )}
-        {collapsed ? (
-          <Center style={{paddingTop:"10px"}}>
-
-        <Button onClick={onClick} style={{minWidth:"100%",height:"100%",color:"black",backgroundColor:"white"}}>▲</Button>
+      )}
+      {collapsed && CollapsedComponent && (
+        <Center style={{ paddingTop: "10px" }}>
+          <Button
+            onClick={onClick}
+            style={{
+              minWidth: "100%",
+              height: "100%",
+              color: "black",
+              backgroundColor: "white",
+            }}
+          >
+            ▲
+          </Button>
         </Center>
-        ) : (
-          <Center style={{paddingTop:"10px"}}>
-        <Button onClick={onClick} style={{minWidth:"100%",height:"100%",color:"black",backgroundColor:"white"}}>▼</Button>
-        </Center>
-        )}
-        
+      )}
     </Paper>
   );
 }
